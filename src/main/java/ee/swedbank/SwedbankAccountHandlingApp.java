df8f473c3dd58db59f2d32ff131b34ecbde76755
talgat.aitchanov <talgat.aitchanov@gmail.com>
2026-03-06 14:15:55 +0200

    Hello world

---
+++ src/main/java/ee/swedbank/SwedbankAccountHandlingApp.java
@@ -0,0 +1,11 @@
+package ee.swedbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwedbankAccountHandlingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SwedbankAccountHandlingApp.class, args);
+    }
+}
